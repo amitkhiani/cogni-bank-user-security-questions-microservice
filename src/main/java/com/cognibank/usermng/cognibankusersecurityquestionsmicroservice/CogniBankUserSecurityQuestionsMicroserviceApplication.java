32e4ffd32de5f585c7c88936d561c3ea42cde4fb
Genzeb <genzebnge@gmail.com>
2019-04-02 11:36:22 -0400

    Initial Setup

---
+++ src/main/java/com/cognibank/usermng/cognibankusersecurityquestionsmicroservice/CogniBankUserSecurityQuestionsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.cognibank.usermng.cognibankusersecurityquestionsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CogniBankUserSecurityQuestionsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CogniBankUserSecurityQuestionsMicroserviceApplication.class, args);
+	}
+
+}
